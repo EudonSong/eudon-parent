f5afb3c769e7c0a481e62f1a47a0d8e7db4ca87e
eudon <syd19970616@gmail.com>
2025-06-04 17:28:50 +0800

    FEAT: update application.yml

--- eudon-service/src/main/java/com/eudon/service/TemplateApplication.java
+++ eudon-service/src/main/java/com/eudon/service/TemplateApplication.java
@@ -3,6 +3,14 @@ package com.eudon.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author eudon
+ * @version <p>1.0</p>
+ * @Description 启动类
+ * @email syd19970616@gmail.com
+ * @repository <a href="https://github.com/EudonSong">GitHub</a>
+ * @since 2025/06/04
+ */
 @SpringBootApplication
 public class TemplateApplication {
     public static void main(String[] args) {
