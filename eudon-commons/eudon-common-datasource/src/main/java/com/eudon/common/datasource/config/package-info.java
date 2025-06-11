0113e5eadc71cbf4fa97dfbf6b69f43a18457e1a
eudon <syd19970616@gmail.com>
2025-06-11 17:13:16 +0800

    FEAT: 更新数据源信息

---
+++ eudon-commons/eudon-common-datasource/src/main/java/com/eudon/common/datasource/config/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author eudon
+ * @version <p>1.0</p>
+ * @Description 数据库配置包
+ * @email syd19970616@gmail.com
+ * @repository <a href="https://github.com/EudonSong">GitHub</a>
+ * @since 2025/6/11
+ */
+package com.eudon.common.datasource.config;
