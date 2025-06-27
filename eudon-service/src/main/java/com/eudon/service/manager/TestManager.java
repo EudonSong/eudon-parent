951d59b305bd1f83ae931538d8def9f0cb5c572f
eudon <syd19970616@gmail.com>
2025-06-27 16:53:54 +0800

    FEAT: update pom struct

--- eudon-service/src/main/java/com/eudon/service/manager/TestManager.java
+++ eudon-service/src/main/java/com/eudon/service/manager/TestManager.java
@@ -6,6 +6,12 @@ import com.eudon.common.framework.annotation.Manager;
 import com.eudon.service.dao.TbUserMapper;
 import com.eudon.service.domain.entity.TbUserEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.CuratorZookeeperClient;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.List;
 
 /**
  * @author eudon
@@ -18,10 +24,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Manager
 public class TestManager {
-
+    private final CuratorFramework client;
     private final TbUserMapper tbUserMapper;
 
-    public TestManager(TbUserMapper tbUserMapper) {
+    public TestManager(CuratorFramework client, TbUserMapper tbUserMapper) {
+        this.client = client;
         this.tbUserMapper = tbUserMapper;
     }
 
@@ -32,5 +39,27 @@ public class TestManager {
         wrapper.setEntity(tbUserEntity);
         TbUserEntity one = tbUserMapper.selectOne(wrapper);
         log.debug("查询结果：{}",one);
+
+        try {
+//            String s = client.create().forPath("/eudon1");
+//            log.info("创建结果:{}",s);
+//            client.setData().forPath("/eudon","eudon".getBytes());
+//            byte[] eudonDate = client.getData().forPath("/eudon");
+//            log.info("eudonData:{}",new String(eudonDate));
+//            List<String> children = client.getChildren().forPath("/");
+//            log.info("/的子节点:{}",children);
+//            byte[] bytes = client.getData().forPath("/services/TemplateApplication");
+//            log.info("/service:{}",bytes);
+            CuratorZookeeperClient zookeeperClient = client.getZookeeperClient();
+            ZooKeeper zooKeeper = zookeeperClient.getZooKeeper();
+            client.delete().inBackground((var1,var2) -> {
+                log.info("curatorFramework:{}",var1);
+                log.info("curatorEvent:{}",var2);
+            }).forPath("/eudon");
+//            byte[] data = zooKeeper.getData("/", true, new Stat());
+//            log.info("zookeeper:{}",data);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
